b6221bef007886b69185c0af0403a943fa8c7583
Michael9921 <101097176+Michael9921@users.noreply.github.com>
2022-12-15 22:54:58 +0700

    Delete server directory

--- server/app/model/dataLogs.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const dataLogs = mongoose.model('datalogs', new mongoose.Schema({
-    idNode: String,
-    waterLevel: Number,
-    timestamp: String,
-}));
-
-module.exports = dataLogs;
